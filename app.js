577db09c4230e87029bc68a767bb41ae20e307de
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-19 12:15:39 +0000

    Support creating users

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const express = require("express");
-
 const app = express();
+const path = require("node:path");
+const usersRouter = require("./routes/usersRouter");
 
-const indexRouter = require("./routers/indexRouter");
-const notFound = require("./middlewares/notFound");
-const errorHandler = require("./middlewares/serverError");
-
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", usersRouter);
 
-app.use("/", indexRouter);
-app.use(notFound);
-app.use(errorHandler);
-
-const PORT = 3000;
-app.listen(PORT, (err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(`Server up and running at port: ${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
     }
+    console.log(`Express app listening on port ${PORT}!`);
 });
--- routers/indexRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (_, res) => {
-    res.render("index");
-});
-
-module.exports = router;
